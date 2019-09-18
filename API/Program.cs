faa7d3aac997c20d30e2d601fc381ee0124e47e9
Nick Nie <sky_elbor@hotmail.com>
2019-09-18 16:54:43 +1000

    Initial check-in of module Activities

---
+++ API/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var provider = scope.ServiceProvider;
+                try
+                {
+                    var dataContext = provider.GetRequiredService<DataContext>();
+                    dataContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = provider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+            host.Run();
+            //CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>(builder =>
+            {
+                builder.HasData(
+                    new Value { Id = 1, Name = "value 101" },
+                    new Value { Id = 2, Name = "value 102" },
+                    new Value { Id = 3, Name = "value 103" }
+                );
+            });
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
